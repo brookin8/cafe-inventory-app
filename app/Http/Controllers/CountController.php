0d4fb48aae075d70268e4f2d24d7e49e97ec0564
Erin Brookins Wilcox <brookin8@gmail.com>
2017-12-03 10:45:11 -0500

    troubleshooting inventory count controller

--- app/Http/Controllers/CountController.php
+++ app/Http/Controllers/CountController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
-class InventoryCountController extends Controller
+class CountController extends Controller
 {
     
      /**
